1ce691d3d8dc2455e9163eba7e7c9dd1c3f4c7f2
ahmed <ahmedbenyahia654@gmail.com>
2025-09-17 14:38:34 +0200

    fixed cors

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import coverLetterRoutes from "./routes/motivationRoutes.js";
 import path from "path";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 // Serve static files
